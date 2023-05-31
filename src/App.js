3f5c95465b616005487e7061001d1b7284f5013e
Amrendra Pratap Singh <singhamrendrapratap19@gmail.com>
2023-05-31 23:40:09 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import { useRef, useState } from "react";
 import './App.css';
+import axios from "axios";
 
 function App() {
+
+  const [result, setResult] = useState("");
+  const [uploadedImgSrc, setUploadedImgSrc] = useState("");
+  const inputRef = useRef();
+  
+  const callApi = async () => {
+
+    const file = inputRef.current.files[0];
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      axios({
+        method: "POST",
+        url: "https://detect.roboflow.com/model/version",
+        params: {
+            api_key: ""
+        },
+        data: reader.result,
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+      })
+      .then(function(response) {
+        setResult(response.data);
+        setUploadedImgSrc(URL.createObjectURL(file));
+      })
+      .catch(function(error) {
+        console.log(error.message);
+      });
+    };
+    reader.readAsDataURL(file);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input ref = {inputRef} type="file"></input>
+      <br></br>
+      <button onClick={callApi}>Submit</button>
+      <br></br>
+      <div>{JSON.stringify(result)}</div>
+      <img src={uploadedImgSrc} alt="imge"></img>
     </div>
   );
 }
